f6196e1381f6384e486132492471050266b12df9
MohitLabs <mohitsingh5.ms@gmail.com>
2023-06-11 21:15:16 +0530

    commit

---
+++ app/src/main/java/com/mohitlabs/yakshini/APIKEYS.java
@@ -0,0 +1,5 @@
+package com.mohitlabs.yakshini;
+
+public interface APIKEYS {
+    String GPTKEY = "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";
+}
--- app/src/main/java/com/mohitlabs/yakshini/MainActivity.java
+++ app/src/main/java/com/mohitlabs/yakshini/MainActivity.java
@@ -23,32 +23,46 @@
 package com.mohitlabs.yakshini;
 
 import android.Manifest;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
-import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.Environment;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.io.File;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static final int MICROPHONE_PERMISSION_CODE = 1048;
     SpeechRecognizer speechRecognizer;
     MediaPlayer mediaPlayer;
     private boolean isListening = true;
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    private OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,14 +80,15 @@ public class MainActivity extends AppCompatActivity {
 
         listenBtn.setOnClickListener(view -> {
             if (isListening) {
-                listenBtn.setImageDrawable(getDrawable(R.drawable.ic_baseline_mic_24));
+                listenBtn.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.ic_baseline_mic_24));
                 speechRecognizer.startListening(speechRecognizerIntent);
                 isListening = false;
             } else {
-                listenBtn.setImageDrawable(getDrawable(R.drawable.ic_baseline_mic_off_24));
+                listenBtn.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.ic_baseline_mic_off_24));
                 speechRecognizer.stopListening();
                 isListening = true;
             }
+            callGPTAPI("Hello!");
         });
 
         speechRecognizer.setRecognitionListener(new RecognitionListener() {
@@ -103,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onResults(Bundle bundle) {
-                ArrayList<String> data = bundle.getStringArrayList(speechRecognizer.RESULTS_RECOGNITION);
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
                 Toast.makeText(getApplicationContext(), data.get(0), Toast.LENGTH_LONG).show();
             }
 
@@ -132,5 +147,56 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, MICROPHONE_PERMISSION_CODE);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MICROPHONE_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void callGPTAPI(String question) {
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model", "text-davinci-003");
+            jsonBody.put("prompt", question);
+            jsonBody.put("max_tokens", 4000);
+            jsonBody.put("temperature", 0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization", "Bearer " + APIKEYS.GPTKEY)
+                .post(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Toast.makeText(getApplicationContext(), "Failed to get response " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        Log.d("GPTAPI", result.trim());
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    Log.d("GPTAPI", "Failed to get response due to " + response.body().toString());
+                }
+            }
+        });
     }
 }
